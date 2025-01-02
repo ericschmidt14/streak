cfce2fa88d749426d7f2b1d3b991b4ffc0a15ba6
E R I C S C H M I D T <info@ericschmidt.de>
2025-01-02 12:53:17 +0100

    add dynamic greeting

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,13 +12,32 @@ import {
 export default function Header() {
   const { longestStreak, currentStreak } = useStreakContext();
 
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 0 && currentHour < 6) {
+      return "Good early morning,";
+    } else if (currentHour >= 6 && currentHour < 10) {
+      return "Good morning,";
+    } else if (currentHour >= 10 && currentHour < 11) {
+      return "Morning,";
+    } else if (currentHour >= 11 && currentHour < 13) {
+      return "Hi";
+    } else if (currentHour >= 13 && currentHour < 17) {
+      return "Good afternoon,";
+    } else if (currentHour >= 17 && currentHour < 21) {
+      return "Good evening,";
+    } else {
+      return "Good night,";
+    }
+  };
+
   return (
     <header
       className={`sticky -top-20 z-50 w-full flex flex-col ${defaultShadow} ${backdropBlur} ${borderBottom}`}
     >
       <div className={`flex justify-between items-center ${defaultPadding}`}>
         <h1 className="font-medium text-lg leading-tight">
-          Hi Eric, <br /> It&apos;s a good day to go for a run.
+          {getGreeting()} Eric. <br /> It&apos;s a good day to go for a run.
         </h1>
         <Image
           src="/profile.jpeg"
