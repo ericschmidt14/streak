73815683712051f9e155451377f68d9f51a34470
E R I C S C H M I D T <info@ericschmidt.de>
2025-01-06 12:41:08 +0100

    tweak graph

--- src/app/components/Year.tsx
+++ src/app/components/Year.tsx
@@ -22,16 +22,16 @@ export default function Year({ year }: { year: number }) {
 
   return (
     <div className="flex flex-col gap-2">
-      <header className="flex justify-between items-end gap-2">
+      <header className="flex justify-between items-end gap-4">
         <h2 className="text-4xl font-bold tracking-tighter">{year}</h2>
         <Sparkline
           curveType="monotone"
           color="red.5"
           data={streakHistory}
           fillOpacity={0.6}
-          strokeWidth={1.4}
-          w={180}
-          h={36}
+          strokeWidth={1}
+          w={9999}
+          h={44}
         />
       </header>
       <div className="grid grid-cols-[repeat(13,1fr)] lg:grid-cols-1">
